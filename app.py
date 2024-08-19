3ffeb971a64ac8a7bf8193015f5ce9bb2ae6d970
Harasiddh A R <harasiddhsavitrika@gmail.com>
2024-08-19 15:52:42 +0530

    Improved the home page UI

--- app.py
+++ app.py
@@ -3,9 +3,25 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def home():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/shop')
+def shop():
+    # You can add logic to fetch and display items for sale here
+    return render_template('shop.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
